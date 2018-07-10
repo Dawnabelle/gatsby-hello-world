9bb8bec33fdb0cc8ad64738b4035b29fee5b95f0
Guest User <Guest@Epicodus-8A.local>
2018-07-10 14:18:50 -0700

    initial gatsby files and folders

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default () =>
+  <div style={{
+      color: 'teal'
+    }}>
+    <h1>Hello Gatsby!</h1>
+    <p>Will this be fun to use to build my Portfolio for my Capstone project?</p>
+    <p>Results in a bit</p>
+    <img src='https://images.unsplash.com/photo-1504105547124-fc1aee34470d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5776563a3b4df93f2729a29e9bf5c27b&auto=format&fit=crop&w=1352&q=80' alt='retro room' width='500' />
+  </div>
